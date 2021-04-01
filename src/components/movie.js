45b460c8d043d1a0fb8434bdc56895ba10fa2369
Ryan Ellis <ryan.ellis33@gmail.com>
2021-03-31 18:41:18 -0600

    second Attempt

---
+++ src/components/movie.js
@@ -0,0 +1,22 @@
+import React, { useState }  from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { fetchMovie } from "../actions/movieActions";
+import MovieDetail from "../components/moviedetail"
+
+//support routing by creating a new component
+
+function Movie(props) {
+    const [selectedMovie] = useState(props.selectedMovie);
+    const params = useParams();
+    const movieTitle = params.movie_title;
+    console.log(movieTitle);
+    const dispatch = useDispatch();
+    if (selectedMovie == null) {
+        dispatch(fetchMovie(movieTitle));
+    }
+
+    return (<MovieDetail movieTitle={movieTitle} />)
+}
+
+export default Movie;
